0dcacdd2565b605c186bb8430485dcd4627e0bf1
d2lee <d2lee@users.noreply.github.com>
2020-08-02 20:12:05 +0900

    Formatting meta page

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+
 @app.route('/info')
 def info():
     return render_template('info.html')
@@ -16,11 +16,13 @@ def meta():
     url = "http://169.254.169.254/latest/meta-data/"
 
     meta_list = urllib.request.urlopen(url).read().decode()
-    str = "Metadata\n"
+    str = "<html><body><h2>Metadata</h2><table>"
     for k in meta_list.split('\n'):
         url_with_key = url + k
         v = urllib.request.urlopen(url_with_key).read().decode()
-        str = str + "%s is %s\n" % (k, v)
+        str = str + "<tr><td>%s</td><td>%s</td></tr>" % (k, v)
+
+    str = str + "</table></body></html>"
     return str
 
 if __name__ == '__main__':

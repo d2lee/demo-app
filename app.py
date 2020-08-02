c34bc34247cd959ad4339f96a3bdcb6539f896ba
d2lee <d2lee@users.noreply.github.com>
2020-08-02 21:52:48 +0900

    Adding the load logic

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 import urllib.request
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
+    
 @app.route('/info')
 def info():
     return render_template('info.html')
@@ -15,15 +16,23 @@ def info():
 def meta():
     url = "http://169.254.169.254/latest/meta-data/"
 
-    meta_list = urllib.request.urlopen(url).read().decode()
-    str = "<html><body><h2>Metadata</h2><table>"
-    for k in meta_list.split('\n'):
+    metadata_list = urllib.request.urlopen(url).read().decode()
+    str = "Metadata\n"
+    for k in metadata_list.split('\n'):
         url_with_key = url + k
         v = urllib.request.urlopen(url_with_key).read().decode()
-        str = str + "<tr><td>%s</td><td>%s</td></tr>" % (k, v)
-
-    str = str + "</table></body></html>"
+        str = str + "%s is %s\n" % (k, v)
     return str
 
+@app.route('/load')
+def load():
+    idleCpu = int(os.popen('vmstat 1 2 | awk \'{ for (i=1; i<=NF; i++) if ($i=="id") { getline; getline; print $i }}\'').read())
+    
+    if idleCpu > 50:
+        os.system('dd if=/dev/zero bs=100M count=500 | gzip | gzip -d  > /dev/null &')
+        return '<p>Current CPU load is %s</p><p>Generating CPU Load!</p>' % str(100 - idleCpu)
+    else:
+        return '<p>Current CPU load is %s</p><p>Under High CPU Load!</p>' % str(100 - idleCpu)
+        
 if __name__ == '__main__':
     app.run()

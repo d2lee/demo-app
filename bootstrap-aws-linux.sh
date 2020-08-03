1e9ac19940228d5cf889654cb877cac590f187a3
d2lee <d2lee@users.noreply.github.com>
2020-08-03 23:46:48 +0900

    Make the nginx and gunicorn to run at daemon

--- bootstrap-aws-linux.sh
+++ bootstrap-aws-linux.sh
@@ -2,31 +2,50 @@
 
 yum update -y
 
-amazon-linux-extras install nginx1 python3.8 -y
+amazon-linux-extras install nginx1 python3.8 -y 
 
-cat << 'EOF' >> /tmp/example.com
+cat << 'EOF' >> /etc/nginx/conf.d/demo.conf
 server {
         listen 80;
-        access_log  /var/log/nginx/example.log;
+        access_log  /var/log/nginx/demo.log;
         location / {
-                proxy_pass http://127.0.0.1:8000/;
+                proxy_pass http://127.0.0.1:8000;
                 proxy_set_header Host $host;
                 proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         }
 }
 EOF
 
-mv /tmp/example.com /etc/nginx/conf.d/example.com.conf
-
 # Restart nginx to make it effect
-systemctl restart nginx
+systemctl start nginx
+systemctl enable nginx
 
 yum install python3-pip git -y
 
 # Install flask
-pip3 install flask gunicorn
+pip3 install flask gunicorn boto3
 
 # Git clone to fetch the source
 su - ec2-user -c "cd /home/ec2-user && git clone https://github.com/d2lee/demo-app.git"
 
-cd /home/ec2-user/demo-app && gunicorn app:app
+# Create the gunicorn config file
+cat << 'EOF' >> /etc/systemd/system/gunicorn.service
+[Unit]
+Description=gunicorn daemon
+After=network.target
+
+[Service]
+User=ec2-user
+Group=ec2-user
+WorkingDirectory=/home/ec2-user/demo-app
+ExecStart=/usr/local/bin/gunicorn \
+        --workers 3 \
+        --bind 127.0.0.1:8000 \
+        app:app
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl restart gunicorn
+systemctl enable gunicorn
